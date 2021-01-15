0e2b4732970a890d23bb0e68d27264f45eac441d
jhonelee <liyunjiao2048@gmail.com>
2021-01-15 22:35:43 +0800

    init

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('assert');
+const Router = require('..');
+
+describe('test/index.test.js', () => {
+  it('should expose Router', () => {
+    assert(typeof Router === 'function');
+    assert(typeof Router.KoaRouter === 'function');
+    assert(typeof Router.EggRouter === 'function');
+  });
+});
